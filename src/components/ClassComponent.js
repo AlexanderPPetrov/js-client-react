756b9e14dbd5628b0504df4f011bd692276a505f
Aleksander Petrov <apetrovdev@gmail.com>
2019-12-23 11:22:02 +0200

    initial commit

---
+++ src/components/ClassComponent.js
@@ -0,0 +1,9 @@
+import React, {Component} from "react";
+
+class ClassComponent extends Component {
+    render() {
+        return <div>Class Component Example</div>
+    }
+}
+
+export default ClassComponent;
---
+++ src/components/FunctionalComponent.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const FunctionalComponent = () => {
+    return <div>
+        Functional Component Example
+    </div>
+};
+
+export default FunctionalComponent;
