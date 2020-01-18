280f2c9865b242162294d012e77c4896cfbfee2a
Ivan Zhelev <nvjcompany@gmail.com>
2020-01-18 11:01:32 +0200

    classes

--- src/components/singleMovie/SingleMovie.js
+++ src/components/singleMovie/SingleMovie.js
@@ -13,14 +13,14 @@ class SingleMovie extends Component {
                     <img className="movie-image" 
                     src="https://image.tmdb.org/t/p/w370_and_h556_bestv2/fn5hrKQNVv3NsXim3pHN0ZztPj5.jpg"/>
                     <div className="p-3 d-flex flex-column">
-                        <div>
+                        <div className="pb-3">
                             <h4>Movie title</h4>
                             <p>2939.20203.2</p>
                             <div className="mt-3 movie-description">
                                 Сюжетът разказва историята на Рой МакБрайд – космически инженер. Бащата на МакБрайд е изчезнал безследно преди 20 год., по време на космическа експедиция към Не…
                             </div>
                         </div>
-                        <div className="mt-auto border-top p-3">
+                        <div className="mt-auto border-top pt-3">
                             <button type="button" 
                             className="btn btn-link">
                                 Повече Информация
